424db9a8c8ab9e935368dcdf382cddf7bb5e6e76
Michele Sciabarra <michele@nuvolaris.io>
2024-02-09 15:20:40 +0000

    work-in-progress

---
+++ packages/azure/completions.js
@@ -0,0 +1,33 @@
+//--web true
+//--kind nodejs:default
+//--param OPENAI_API_KEY $OPENAI_API_KEY
+//--param OPENAI_API_HOST $OPENAI_API_HOST
+
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
+
+async function main(args) {
+
+    const key =  args.OPENAI_API_KEY || process.env.OPENAI_API_KEY
+    const host = args.OPENAI_API_HOST ||  process.env.OPENAI_API_HOST
+    const model =  "gpt-35-turbo"
+    const AI  = new OpenAIClient(host, new AzureKeyCredential(key))
+    const input = args.input || ""
+    let output = "Please provide some input."
+    if(input != "") {
+        //const { id, created, choices, usage } =
+        let request= [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: input },
+        ]
+        
+        let r = await AI.getChatCompletions(model, request);
+
+    } 
+  
+    AI1.models.list()
+    .list().model_dump()
+    return { models: data }
+}
+
+
+
